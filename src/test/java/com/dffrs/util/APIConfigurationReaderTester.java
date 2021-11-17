ad00e7c9d405a4dc13c846e25ff9d6876e50a133
Daniel Rodrigues <dffrs@iscte-iul.pt>
2021-11-17 14:41:22 +0000

    Implemented a new test to check how APIConfigurationReaderTester behaves when an empty configuration file is passed, as an argument.

--- src/test/java/com/dffrs/util/APIConfigurationReaderTester.java
+++ src/test/java/com/dffrs/util/APIConfigurationReaderTester.java
@@ -11,33 +11,44 @@ import java.util.Map;
 public class APIConfigurationReaderTester {
     URL testFile = getClass().getResource("/testFile.txt");
     URL testFileWithMissingParameters = getClass().getResource("/testFileWithMissingParameters.txt");
+    URL testFileEmpty = getClass().getResource("/testFileEmpty.txt");
+
     APIConfigurationReader reader;
     String normalPath = "";
     String missingParametersPath = "";
+    String emptyParametersPath = "";
 
     @Before
     public void init() throws URISyntaxException {
         normalPath = testFile.toURI().getPath();
         missingParametersPath = testFileWithMissingParameters.toURI().getPath();
+        emptyParametersPath = testFileEmpty.toURI().getPath();
+
         reader = APIConfigurationReader.getInstance(normalPath);
     }
 
     @Test
-    public void getConfigurationParametersLoadConfirmationTest(){
-        for(Object co : APIConfigurationReader.getConfigurationsParameters().values())
+    public void getConfigurationParametersLoadConfirmationTest() {
+        for (Object co : APIConfigurationReader.getConfigurationsParameters().values())
             Assert.assertNotNull(co);
     }
 
     @Test
-    public void getConfigurationsTest(){
+    public void getConfigurationsTest() {
         Map<String, String> aux = reader.getConfigurations();
         Assert.assertFalse(aux.isEmpty());
     }
 
     @Test
-    public void getConfigurationsWithMissingParametersTest(){
+    public void getConfigurationsWithMissingParametersTest() {
         reader = APIConfigurationReader.getInstance(missingParametersPath);
         Map<String, String> auxWithNULLValues = reader.getConfigurations();
         Assert.assertTrue(auxWithNULLValues.size() != APIConfigurationReader.getConfigurationsParameters().size());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void getConfigurationsWithEmptyFileTest() {
+        reader = APIConfigurationReader.getInstance(emptyParametersPath);
+        Map<String, String> aux = reader.getConfigurations();
     }
 }
