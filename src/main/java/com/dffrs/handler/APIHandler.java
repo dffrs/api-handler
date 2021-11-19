d416ee985a50cce5351a49305883f8c37ecabed2
dffrs <dffrs@iscte-iul.pt>
2021-11-19 11:35:01 +0000

    Early implementation of an API call. Lot of unfinished and polished code, but, for now, it works.
    Future code cleanup needed.
    
    Also, change to Java 10, inside pom.xml.

--- src/main/java/com/dffrs/handler/APIHandler.java
+++ src/main/java/com/dffrs/handler/APIHandler.java
@@ -8,13 +8,88 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.net.URLEncoder;
-import java.util.Deque;
-import java.util.Map;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public final class APIHandler {
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
     private static APIHandler instance;
-    private static final String CONF_FILE = "TEMP";
+    private static String CONF_FILE;
+
+    // TODO: Create an internal class to represent the different type of API Requests
+    public static class Request {
+
+        private final List<String> params;
+        private final List<String> values;
+        private String query;
+
+        public Request(List<String> params, List<String> values) {
+            this.params = new ArrayList<>();
+            this.values = new ArrayList<>();
+
+            // This option means the were no params
+            if (params == null) {
+                query = String.format(values.get(0), URLEncoder.encode((values.get(0)), CHARSET));
+            } else {
+                try {
+                    query = prepareQuery(params, values);
+                } catch (NullPointerException e) {
+                    throw new NullPointerException("ERROR: Values's List can not be a NULL REFERENCE.\n");
+                } catch (IllegalStateException e) {
+                    throw new IllegalStateException("ERROR: Params's size =/= Values's size.\n");
+                }
+            }
+        }
+
+        public String getQuery() {
+            return query;
+        }
+
+        //TODO: TEST
+        private String encodeString(String param, String value)  {
+            //TODO: Check to see if parameters, also, need to be encoded.
+            // They dont...
+            String temp = param +"="+(URLEncoder.encode(value, CHARSET));
+            // String temp = (URLEncoder.encode(param +"="+value, CHARSET));
+            return temp;
+        }
+
+        //TODO: TEST
+        private String prepareQuery(List<String> params, List<String> values) throws NullPointerException,
+                IllegalStateException{
+            
+            if (values == null) {
+                throw new NullPointerException();
+            }
+
+            if (params.size() != values.size()) {
+                throw new IllegalStateException();
+            }
+
+            String query = "";
+            for (int i = 0; i != params.size(); i ++) {
+                String tempParams = params.get(i);
+                String tempValues = values.get(i);
+                query += (encodeString(tempParams, tempValues) + "&");
+            }
+            return query.substring(0, query.length() - 1);
+        }
+    }
+
+    static {
+        try {
+            CONF_FILE = ((URL) APIHandler.class.getResource("/configFile.txt")).toURI().getPath();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            throw new NullPointerException("ERROR: Configuration Option file not found.\n");
+        }
+    }
+
     private APIConfigurationReader reader;
     private Map<String, String> configurations;
 
@@ -42,44 +117,24 @@ public final class APIHandler {
     }
 
     //TODO: TEST
-    public void makeAPIRequest(Deque<String> paramaters, Deque<String> values) throws UnirestException {
+    public HttpResponse<JsonNode> makeAPIRequest(APIHandler.Request r) throws UnirestException {
         String host = getAPIParameterBy("host");
         String api_host = getAPIParameterBy("rapid_api_host");
         String api_key = getAPIParameterBy("rapid_api_key");
         String header = getAPIParameterBy("header");
         String endpoint = getAPIParameterBy("endpoint");
-        String query = prepareQuery(paramaters, values);
+        // String query = prepareQuery(paramaters, values);
 
-        HttpResponse<JsonNode> request = Unirest.get(host+"/"+endpoint+"?"+query)
-                .header("x-rapidapi-host", header)
+        //TODO: Hardcode just to test the connection to an API Service
+        // https://car-code.p.rapidapi.com/obd2/P0001
+        String value = "P0001";
+        String apiCall = host+"/"+endpoint+"/"+value;
+
+        HttpResponse<JsonNode> request = Unirest.get(apiCall)
+                .header(header, api_host)
                 .header("x-rapidapi-key", api_key)
                 .asJson();
-    }
-
-    //TODO: TEST
-    public String prepareQuery(Deque<String> params, Deque<String> values) {
-        if (params.isEmpty() || values.isEmpty())
-            throw new IllegalArgumentException();
-        if (params.size() != values.size())
-            throw new IllegalStateException();
-        StringBuilder finalQuery = new StringBuilder();
-
-        while(!params.isEmpty()) {
-            try {
-                finalQuery.append(encodeString(params.pop(), values.pop()));
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                break;
-            }
-        }
-
-        return finalQuery.toString();
-    }
-
-    //TODO: TEST
-    private String encodeString(String param, String value) throws UnsupportedEncodingException {
-        String charset = "UTF-8";
-        return String.format(param+"=%value", URLEncoder.encode(value, charset));
+        return request;
     }
 
     public static APIHandler getInstance(){
---
+++ src/test/java/com/dffrs/handler/APIHandlerTester.java
@@ -0,0 +1,59 @@
+package com.dffrs.handler;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class APIHandlerTester {
+    URL testFile = getClass().getResource("/testFile.txt");
+
+    APIHandler handler;
+    String testFilePath = "";
+
+    @Before
+    public void init() throws URISyntaxException {
+        testFilePath = testFile.toURI().getPath();
+
+        handler = APIHandler.getInstance();
+    }
+
+    @Test
+    public void getAPIParameterByTest() {
+        String option = "host";
+        String result = handler.getAPIParameterBy(option);
+        System.out.println(result);
+
+        Assert.assertFalse(result.isEmpty());
+    }
+
+    @Test
+    public void getQueryWithNoParametersTest() {
+        List<String> valuesList = new ArrayList<>();
+        valuesList.add("P0001");
+
+        String query = new APIHandler.Request(null, valuesList).getQuery();
+        Assert.assertEquals(URLEncoder.encode(valuesList.get(0), StandardCharsets.UTF_8), query);
+    }
+
+    @Test
+    public void makeAPICallWithNoParametersTest() throws UnirestException {
+        List<String> valuesList = List.of("POOO1");
+        HttpResponse<JsonNode> response;
+
+        response = handler.makeAPIRequest(new APIHandler.Request(null, valuesList));
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(200, response.getStatus());
+        Assert.assertNotNull(response.getBody());
+    }
+}
