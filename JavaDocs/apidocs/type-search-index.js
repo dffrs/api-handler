251ae225a8cdd409157ac5748207c7e60a597ae1
Daniel Rodrigues <dffrs@iscte-iul.pt>
2021-11-21 20:51:27 +0000

    Added GitHub's package propriety to pom. Created a folder for JavaDoc.

---
+++ JavaDocs/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.dffrs.util","l":"APIConfigurationReader"},{"p":"com.dffrs.handler","l":"APIHandler"},{"p":"com.dffrs.handler","l":"APIHandler.Request"}]
