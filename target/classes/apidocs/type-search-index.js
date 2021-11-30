feed15020efff4d973ad1e0bd9b8afee853e4401
Daniel <43683330+dffrs@users.noreply.github.com>
2021-11-30 13:37:54 +0000

    Delete target directory

--- target/classes/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.dffrs.util","l":"APIConfigurationReader"},{"p":"com.dffrs.handler","l":"APIHandler"},{"p":"com.dffrs.handler","l":"APIHandler.Request"}]
